ecc44aecdc08f6dbe7764b1292f3d2633eb13bc2
marczome <marczm090102@gmail.com>
2023-10-25 10:32:11 +0200

    Script funcional abrir y cerrar puerta

--- Assets/Scripts/porta.cs
+++ Assets/Scripts/porta.cs
@@ -1,18 +1,54 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class porta : MonoBehaviour
 {
+    public Transform puntoderotacion;
+    private bool abierto = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public void abrir()
+    {
+        Vector3 ejerotacion = puntoderotacion.up; // Eje en el que rota
+        Vector3 puntorotacion = puntoderotacion.position; // Posici�n del objeto hijo para rotar
+        transform.RotateAround(puntorotacion, ejerotacion, 20 * Time.deltaTime);
+        abierto = false;
+        StartCoroutine(movewithDelay());
+    }
+
+    public void close()
+    {
+        Vector3 ejerotacion = puntoderotacion.up; // Eje en el que rota
+        Vector3 puntorotacion = puntoderotacion.position; // Posici�n del objeto hijo para rotar
+        transform.RotateAround(puntorotacion, ejerotacion, -20 * Time.deltaTime);
+    }
+
+    IEnumerator movewithDelay() //rutina
+    {
+        yield return new WaitForSeconds(8);
+        close();
+        abierto = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float eulerAng = transform.rotation.eulerAngles.y;
+        if (!abierto && eulerAng < 90f)
+        {
+            abrir();
+        }
+
+        if (abierto && eulerAng <= 0f)
+        {
+            abrir();
+        }
+
     }
 }
